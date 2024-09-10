81816d5dc5cd5ffd27a750010c2b7d5dc174b7aa
Esther Cheng <ezc3@duke.edu>
2024-09-10 17:27:02 -0400

    changed image tags

--- src/pages/myinterests.tsx
+++ src/pages/myinterests.tsx
@@ -11,28 +11,28 @@ export default function Interests() {
         <div className="appears">
             <h2>Here are some of my recent crochet projects!</h2>
             <div className="crochetphotos">
-                <img src="../img/brownbluesweater.jpeg" className="interestssammy" />
-                <img src="../img/crochet_boba.png" className="interestssammy" />
-                <img src="../img/cow.png" className="interestssammy" />
-                <img src="../img/owl.png" className="interestssammy" />
-                <img src="../img/daisy_bag.jpeg" className="interestssammy" />
-                <img src="../img/dragon.jpeg" className="interestssammy" />
-                <img src="../img/penguin.jpeg" className="interestssammy" />
-                <img src="../img/axolotl.jpeg" className="interestssammy" />
-                <img src="../img/squid.jpeg" className="interestssammy" />
-                <img src="../img/mini_bee.jpeg" className="interestssammy" />
+                <Image src="../img/brownbluesweater.jpeg" alt="crochet" className="interestssammy" />
+                <Image src="../img/crochet_boba.png" alt="crochet" className="interestssammy" />
+                <Image src="../img/cow.png" alt="crochet" className="interestssammy" />
+                <Image src="../img/owl.png" alt="crochet" className="interestssammy" />
+                <Image src="../img/daisy_bag.jpeg" alt="crochet" className="interestssammy" />
+                <Image src="../img/dragon.jpeg" alt="crochet" className="interestssammy" />
+                <Image src="../img/penguin.jpeg" alt="crochet" className="interestssammy" />
+                <Image src="../img/axolotl.jpeg" alt="crochet" className="interestssammy" />
+                <Image src="../img/squid.jpeg" alt="crochet" className="interestssammy" />
+                <Image src="../img/mini_bee.jpeg" alt="crochet" className="interestssammy" />
             </div>
 
             <h2>This is my dog, Sammy!</h2>
             <div className="sammyphotos">
-                <img src="../img/sammy_bandana.jpeg" className="interestssammy" />
-                <img src="../img/sammy_laying.jpeg" className="interestssammy" />
-                <img src="../img/sammy_smiling.jpg" className="interestssammy" />
-                <img src="../img/sammy_sunset.jpeg" className="interestssammy" />
-                <img src="../img/sammy_tiltinghead.jpeg" className="interestssammy" />
-                <img src="../img/sammy_tugging.jpeg" className="interestssammy" />
-                <img src="../img/sammy_with_ball.jpeg" className="interestssammy" />
-                <img src="../img/sammycouch.jpeg" className="interestssammy" />
+                <Image src="../img/sammy_bandana.jpeg" alt="sammy" className="interestssammy" />
+                <Image src="../img/sammy_laying.jpeg" alt="sammy" className="interestssammy" />
+                <Image src="../img/sammy_smiling.jpg" alt="sammy" className="interestssammy" />
+                <Image src="../img/sammy_sunset.jpeg" alt="sammy" className="interestssammy" />
+                <Image src="../img/sammy_tiltinghead.jpeg" alt="sammy" className="interestssammy" />
+                <Image src="../img/sammy_tugging.jpeg" alt="sammy" className="interestssammy" />
+                <Image src="../img/sammy_with_ball.jpeg" alt="sammy" className="interestssammy" />
+                <Image src="../img/sammycouch.jpeg" alt="sammy" className="interestssammy" />
             </div>
         </div>
     </div> 
