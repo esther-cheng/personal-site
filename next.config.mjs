117a435bd5aa8007ff53711f689f7990cc2af2f0
Esther Cheng <144499944+esther-cheng@users.noreply.github.com>
2024-09-10 14:18:34 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/personal-site",
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
-module.exports = nextConfig;
+export default nextConfig;
