af419c6c64900c9c5638901720f73a938c81df01
Esther Cheng <ezc@duke.edu>
2025-12-06 00:08:33 -0500

    trying new things

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/personal-site", // important for serving from a subpath
-  assetPrefix: "/personal-site/", // ensures _next assets are requested from /personal-site/_next/...
-  trailingSlash: true, // makes exported pages use folder/index.html (helpful on GH Pages)
-  images: {
-    unoptimized: true,
-  },
-  // If you are using Next 13+ and want static export:
-  // output: 'export',  // uncomment only if you want Next to use the static export output format
+  output: "export",
+  trailingSlash: true,
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
